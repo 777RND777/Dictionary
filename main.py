882c871cb82a7c0f772e9193e909d0a54a289ae1
Nurmukhanbet Rakhimbayev <rakhimbayev.n@tntsecure.kz>
2020-06-27 16:48:11 +0600

    basic word check was added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from database import *
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMessageBox, QLineEdit, QPushButton, QVBoxLayout, QWidget
+from random import randint
 import sys
 
 
@@ -14,7 +15,9 @@ class MainWindow(QWidget):
 
         self.answerWord = QLineEdit()
 
-        self.submitButton = QPushButton("Submit")
+        self.is_game = False
+        self.submitButton = QPushButton("Start")
+        self.submitButton.clicked.connect(self.is_right)
 
         self.mainLayout = QVBoxLayout()
         self.mainLayout.addWidget(self.questionWord, alignment=Qt.AlignBaseline)
@@ -22,6 +25,29 @@ class MainWindow(QWidget):
         self.mainLayout.addWidget(self.submitButton, alignment=Qt.AlignBaseline)
 
         self.setLayout(self.mainLayout)
+
+    def is_right(self):
+        if self.is_game:
+            if self.answerWord.text() == self.get_translation():
+                print("Right")
+                self.answerWord.setText("")
+                self.set_question_word()
+            else:
+                QMessageBox.critical(None, 'Wrong', 'Right translation is ' + adjectives[self.questionWord.text()])
+                self.submitButton.setText("Start")
+        else:
+            self.submitButton.setText("Submit")
+            self.is_game = True
+            self.set_question_word()
+
+    def set_question_word(self):
+        rnd = randint(0, len(adjectives) - 1)
+        self.questionWord.setText(adjectives[rnd].eng)
+
+    def get_translation(self):
+        for adjective in adjectives:
+            if adjective.eng == self.questionWord.text():
+                return adjective.rus
 
 
 def catch_exceptions(t, val, tb):
