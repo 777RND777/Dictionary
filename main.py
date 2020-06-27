c055043f97930090584ba43e252cad2562c31959
Nurmukhanbet Rakhimbayev <rakhimbayev.n@tntsecure.kz>
2020-06-27 15:58:56 +0600

    basic layout was added

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMessageBox, QWidget
-import os
+from PyQt5.QtWidgets import QApplication, QMessageBox, QLineEdit, QPushButton, QVBoxLayout, QWidget
 import sys
 
 
 class MainWindow(QWidget):
     def __init__(self, **kwargs):
         super(MainWindow, self).__init__(**kwargs)
+        self.setWindowTitle("Dictionary")
+
+        self.questionWord = QLineEdit()
+        self.questionWord.setReadOnly(True)
+
+        self.answerWord = QLineEdit()
+
+        self.submitButton = QPushButton("Submit")
+
+        self.mainLayout = QVBoxLayout()
+        self.mainLayout.addWidget(self.questionWord, alignment=Qt.AlignBaseline)
+        self.mainLayout.addWidget(self.answerWord, alignment=Qt.AlignBaseline)
+        self.mainLayout.addWidget(self.submitButton, alignment=Qt.AlignBaseline)
+
+        self.setLayout(self.mainLayout)
 
 
 def catch_exceptions(t, val, tb):
